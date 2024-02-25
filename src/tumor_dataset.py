6c3c8605d35cdbd77d037658809cf0953dbd3971
John Henry Rudden <johnhenryrudden@gmail.com>
2024-02-25 17:46:22 -0500

    wip semantic segmentation

--- src/tumor_dataset.py
+++ src/tumor_dataset.py
@@ -1,5 +1,5 @@
 import os
-from PIL import Image
+from PIL import Image, ImageDraw
 from torch.utils.data import Dataset
 from enums import DataSplit
 
@@ -31,3 +31,52 @@ class TumorClassificationDataset(Dataset):
             image = self.transform(image)
 
         return image, label
+
+class TumorSemanticSegmentationDataset(Dataset):
+    def __init__(self, root_dir, split: DataSplit, transform=None):
+        self.root_dir = os.path.join(root_dir, 'tumor-segmentation', split.lower())
+        self.transform = transform
+         # Load annotations
+        with open(os.path.join(root_dir, '_annotations.coco.json'), 'r') as file:
+            self.labels = json.load(file)
+
+        # Map image IDs to file names
+        self.image_id_to_file_name = {image['id']: image['file_name'] for image in self.labels['images']}
+
+        # Map image IDs to annotations
+        self.image_id_to_annotation = {}
+        for annotation in self.labels['annotations']:
+            image_id = annotation['image_id']
+            # TODO: may need iscrowd field (not sure what it is)
+            self.image_id_to_annotation[image_id] = {'bbox': np.array(annotation['bbox']), 'segmentation': np.array(annotation['segmentation']).reshape(-1, 2)}
+
+
+        self.image_ids = list(self.image_id_to_file_name.keys())
+
+
+    def __len__(self):
+        return len(self.image_ids)
+
+    def __getitem__(self, idx):
+        image_id = self.image_ids[idx]
+        img_path = os.path.join(self.root_dir, self.image_id_to_file_name[image_id])
+        
+        # TODO: determine if this should be RGB or L (grey scale)
+        image = Image.open(img_path).convert('RGB')
+        annotation = self.image_id_to_annotation[image_id]
+
+        mask = self._create_mask(annotation, image.size)
+
+        if self.transform:
+            image = self.transform(image)
+            mask = self.transform(mask)
+        
+        return image, mask
+    
+    def _create_mask(self, annotation, image_size):
+        mask = Image.new('L', image_size)
+        for seg in annotation['segmentation']:
+            ImageDraw.Draw(mask).polygon(seg.tolist(), outline=1, fill=1)
+        mask = torch.tensor(np.array(mask), dtype=torch.float32)
+        return mask
+            
