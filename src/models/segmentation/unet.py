e7f9883e501cce0077e943b8102f84d7bd3bbc91
John Henry Rudden <johnhenryrudden@gmail.com>
2024-03-05 23:33:29 -0500

    add basic model scaffolding

---
+++ src/models/segmentation/unet.py
@@ -0,0 +1,17 @@
+# Implementation of the U-Net model inspired by the original paper: https://arxiv.org/pdf/1505.04597.pdf
+# The model is implemented using the PyTorch framework.
+
+import torch
+from torch import nn
+
+class UNet(nn.Module):
+    def __init__(self):
+        super(UNet, self).__init__()
+
+        raise NotImplementedError('UNet model not implemented yet.')
+    
+    def forward(self, x):
+        raise NotImplementedError('UNet model not implemented yet.')
+    
+    def backward(self, x):
+        raise NotImplementedError('UNet model not implemented yet.')
