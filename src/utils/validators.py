4e7b53f308107a3377e22d68d15c92201462b0c8
John Henry Rudden <johnhenryrudden@gmail.com>
2024-03-08 14:09:41 -0500

    update validators for flexibility

--- src/utils/validators.py
+++ src/utils/validators.py
@@ -1,5 +1,6 @@
 import torch
 from functools import wraps
+from typing import Union, List
 
 def validate_model_input(checks):
     def decorator(func):
@@ -7,10 +8,10 @@ def validate_model_input(checks):
         def wrapper(self, x, *args, **kwargs):
             if not isinstance(x, torch.Tensor):
                 raise TypeError("Input must be a torch.Tensor")
-            if 'shape' in checks and x.shape[1:] != checks['shape']:
-                raise ValueError(f"Input tensor must have shape {checks['shape']}, but got {x.shape[1:]}") # this ignores the batch size
-            if 'dims' in checks and x.ndim != checks['dims']:
-                raise ValueError(f"Expected input to have {checks['dims']} dimensions, but got {x.ndim}")
+            if 'shape' in checks:
+                validate_shape(x, checks['shape'])
+            if 'dims' in checks:
+                validate_dims(x, checks['dims'])
             if 'dtype' in checks and x.dtype != checks['dtype']:
                 raise TypeError(f"Expected input dtype {checks['dtype']}, but got {x.dtype}")
             if 'device' in checks and x.device != next(self.parameters()).device:
@@ -18,3 +19,23 @@ def validate_model_input(checks):
             return func(self, x, *args, **kwargs)
         return wrapper
     return decorator
+
+
+def validate_shape(x: torch.Tensor, shape: tuple):
+    """
+    Validate that the input tensor has the expected shape
+
+    If the input tensor has a batch dimension, it is ignored (assumes 4D tensor with batch dimension)
+    """
+    if x.shape != shape and not(len(shape) + 1 == x.ndim and shape == x.shape[1:]):
+        raise ValueError(f"Input tensor must have shape {shape}, but got {x.shape[1:]}")
+        
+def validate_dims(x: torch.Tensor, dims: Union[int, List]):
+    """
+    Validate that the input tensor has the expected number of dimensions
+    """
+    if x.ndim == dims:
+        return
+    if isinstance(dims, list) and x.ndim in dims:
+        return
+    raise ValueError(f"Input tensor must have {dims} dimensions, but got {x.ndim}")
