7f6ed94d3584e046478d2aeb9e35dcd46b6678fa
jhrudden <johnhenryrudden@gmail.com>
2024-03-10 19:43:24 -0400

    clean up train script

--- src/scripts/train_segmentation.py
+++ src/scripts/train_segmentation.py
@@ -3,20 +3,113 @@ from torch.utils.data import DataLoader, SubsetRandomSampler
 import torchvision.transforms as transforms
 from sklearn.model_selection import KFold
 from torchinfo import summary
+from tqdm import trange, tqdm
 import argparse
 import os
-from tqdm import tqdm
+from dataclasses import dataclass, field
+torch.set_printoptions(precision=3)
 
 from src.models.segmentation.unet import UNet
 from src.tumor_dataset import TumorSemanticSegmentationDataset
 from src.utils.visualize import show_images_with_masks
 
+"""
+Key Notes for Later Improvements / Implementations Details:
+- NOTE: UNet paper uses SGD with momentum = 0.99 (this may be a good starting point for hyperparameter tuning)
+- NOTE: do we need a scheduler to adjust learning rate?
+- NOTE: UNet also introduces pre-computed weights for the loss function to handle class imbalance
+- NOTE: BCEWithLogitsLoss is used for binary segmentation tasks (like this one) and is a combination of Sigmoid and BCELoss
+- NOTE: Main benefit of BCEWithLogitsLoss is that it is numerically stable because it uses the log-sum-exp trick
+"""
+
 DATASET_ROOT_DIR = 'src/../datasets'
 
+def get_device():
+    """
+    Get the device to use for training (cuda if available, then mps, then cpu)
+    """
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        return torch.device("mps")
+    else:
+        return torch.device("cpu")
 
-def main():
+@dataclass
+class TrainingConfig:
+    batch_size: int = 1
+    learning_rate: float = 0.01
+    num_epochs: int = 10
+    device: torch.device = field(default_factory=lambda: get_device())
+    dataset_root_dir: str = DATASET_ROOT_DIR
+    num_folds: int = 5
+    random_state: int = 42
+    optimizer: str = 'SGD' 
+    momentum: float = 0.99  
 
-    # parse args
+def train(model: torch.nn.Module, train_dataloader: DataLoader, optimizer: torch.optim.Optimizer, loss_fn: torch.nn.Module, config: TrainingConfig):
+    """
+    Train the model on the training set
+
+    Args:
+    - model (torch.nn.Module): the model to train
+    - train_dataloader (DataLoader): the training set dataloader
+    - optimizer (torch.optim.Optimizer): the optimizer to use
+    - loss_fn (torch.nn.Module): the loss function to use
+    - config (dict): a dictionary containing the training configuration
+    """
+    device, num_epochs = config.device, config.num_epochs
+    model.train()
+    for epoch in range(num_epochs):
+        cumalative_loss = 0
+        with tqdm(total=len(train_dataloader), desc=f'Epoch {epoch+1}/{num_epochs}', unit='batch') as pbar:
+            for batch in train_dataloader:
+                imgs, masks = batch
+                imgs, masks = imgs.to(device), masks.to(device)
+                model.zero_grad()
+                output = model(imgs)
+                loss = loss_fn(output, masks)
+                loss.backward()
+                optimizer.step()
+                cumalative_loss += loss.item()
+
+                accuracy = (output.round() == masks).float().mean()
+                pbar.set_postfix({'Loss': loss.item(), 'Accuracy': accuracy.item()})
+                pbar.update()
+        print(f'Epoch {epoch+1}/{num_epochs} - Avg Loss: {cumalative_loss / len(train_dataloader)}')
+
+def evaluate(model: torch.nn.Module, val_dataloader: DataLoader, loss_fn: torch.nn.Module, config: TrainingConfig):
+    """
+    Evaluate the model on the validation set
+
+    Args:
+    - model (torch.nn.Module): the model to evaluate
+    - dataloader (DataLoader): the validation set dataloader
+    - loss_fn (torch.nn.Module): the loss function to use
+    """
+    device = config['device']
+
+    model.eval()
+
+    with torch.no_grad():
+        cumalative_loss = 0
+        with tqdm(total=len(val_dataloader), desc='Validation', unit='batch') as pbar:
+            for imgs, masks in val_dataloader:
+                imgs, masks = imgs.to(device), masks.to(device)
+                output = model(imgs)
+                loss = loss_fn(output, masks)
+                cumalative_loss += loss.item()
+
+                pbar.set_postfix({'Loss': loss.item()})
+                pbar.update()
+
+        print(f'Validation Loss: {cumalative_loss / len(val_dataloader)}')
+
+def get_config():
+    """
+    Get the training configuration
+    """
+     # parse args
     parser = argparse.ArgumentParser(description="Train a model with specified parameters.")
     parser.add_argument('--batch_size', type=int, default=1,
                         help='Input batch size for training (default: 1)')
@@ -26,28 +119,38 @@ def main():
                         help='Number of epochs to train (default: 10)')
     args = parser.parse_args()
 
-    batch_size = args.batch_size
-    learning_rate = args.learning_rate
-    epochs = args.epochs
+    config = TrainingConfig(batch_size=args.batch_size, learning_rate=args.learning_rate, num_epochs=args.epochs)
+    # validate the configuration
+    assert config.batch_size > 0, 'Batch size must be greater than 0'
+    assert 1 > config.learning_rate > 0, 'Learning rate must be greater than 0'
+    assert config.num_epochs > 0, 'Number of epochs must be greater than 0'
+    assert os.path.exists(config.dataset_root_dir), 'Dataset root directory does not exist'
+    
+    return config
+
+    
+def main():
+    # Get the training configuration
+    config = get_config()
+    device = config.device
 
     # Create Segmentation Dataset instance
-
     # TODO: Here is where augmentation should be added to the dataset
     base_transforms = transforms.Compose([
         transforms.Resize((320, 320)),
         transforms.ToTensor()
     ])
-    # TODO: output masks currently have 1 channel, but the model expects 2 channels
-    train_dataset = TumorSemanticSegmentationDataset(root_dir=DATASET_ROOT_DIR, split='train', transform=base_transforms)
-    test_dataset = TumorSemanticSegmentationDataset(root_dir=DATASET_ROOT_DIR, split='test', transform=base_transforms)
+
+    train_dataset = TumorSemanticSegmentationDataset(root_dir=config.dataset_root_dir, split='train', transform=base_transforms)
+    test_dataset = TumorSemanticSegmentationDataset(root_dir=config.dataset_root_dir, split='test', transform=base_transforms)
 
     print(f'Train dataset length: {len(train_dataset)}')
     print(f'Test dataset length: {len(test_dataset)}')
 
     # Create a KFold instance
-    kfold = KFold(n_splits=5, shuffle=True, random_state=42)
-    
-    # Train the model
+    kfold = KFold(n_splits=config.num_folds, shuffle=True, random_state=config.random_state)
+
+    # Train the model using k-fold cross validation
     for fold, (train_ids, val_ids) in enumerate(kfold.split(train_dataset)):
         print(f'Fold {fold}')
         print('---------------------------')
@@ -56,57 +159,28 @@ def main():
         train_sampler = SubsetRandomSampler(train_ids)
         val_sampler = SubsetRandomSampler(val_ids)
 
-        train_dataloader = DataLoader(train_dataset, batch_size=batch_size, sampler=train_sampler)
-        val_dataloader = DataLoader(train_dataset, batch_size=batch_size, sampler=val_sampler) 
+        train_dataloader = DataLoader(train_dataset, batch_size=config.batch_size, sampler=train_sampler)
+        val_dataloader = DataLoader(train_dataset, batch_size=config.batch_size, sampler=val_sampler) 
 
         # Create a UNet model to train on this fold
         model = UNet()
-        summary(model, input_size=(batch_size, 3, 320, 320))
+        summary(model, input_size=(config.batch_size, 3, 320, 320)) # TODO - programatically get input size
 
-        # TODO: Verify best optimizer
-        # NOTE: Adam seems to perform worse in image classification tasks, then SGD
-        # NOTE: UNet paper uses SGD with momentum = 0.99 (this may be a good starting point for hyperparameter tuning)
-        # NOTE: do we need a scheduler to adjust learning rate?
-        optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.99)
+        # Move the model to the device
+        model.to(device)
 
-        # TODO: Verify best loss function (UNet paper uses cross-entropy loss)
-        # NOTE: UNet uses cross-entropy loss maybe we should use BCEWithLogitsLoss here
-        # NOTE: UNet also introduces pre-computed weights for the loss function to handle class imbalance
-        loss_fn = torch.nn.CrossEntropyLoss()
+        if config.optimizer == 'SGD':
+            optimizer = torch.optim.SGD(model.parameters(), lr=config.learning_rate, momentum=config.momentum)
+        else:
+            raise ValueError(f'Invalid optimizer: {config.optimizer}')
+
+        loss_fn = torch.nn.BCEWithLogitsLoss(reduction='mean') # Binary Cross Entropy with Logits Loss
 
         # Train the model
-        model.train()
-        cumalative_loss = 0
-
-        for epoch in tqdm(total=epochs):
-            print(f'Epoch {epoch}')
-            print('---------------------------')
-            
-            # current batch
-            imgs, masks = next(iter(train_dataloader))
-            show_images_with_masks(imgs, masks, nmax=batch_size)
-
-            model.zero_grad()
-            output = model(imgs)
-            loss = loss_fn(output, masks)
-            loss.backward()
-            optimizer.step()
-            cumalative_loss += loss.item()
-            print(f'Loss: {loss.item()}')
-            
-            assert False, 'stop here'
+        train(model, train_dataloader, optimizer, loss_fn, config)
         
-        # Validate the model
-        model.eval()
-        cumalative_loss = 0
-        with torch.no_grad():
-            for imgs, masks in val_dataloader:
-                output = model(imgs)
-                loss = loss_fn(output, masks)
-                cumalative_loss += loss.item()
-        
-        print(f'Validation Loss: {cumalative_loss / len(val_dataloader)}')
-
+        # Evaluate the model
+        evaluate(model, val_dataloader, loss_fn)
 
 
 if __name__ == '__main__':
