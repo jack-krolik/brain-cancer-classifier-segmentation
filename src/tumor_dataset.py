ede181c5f0fbdd86d668eb2190094ec80f7656cb
jhrudden <johnhenryrudden@gmail.com>
2024-03-10 21:17:33 -0400

    remove images with bad annotations from torch dataset

--- src/tumor_dataset.py
+++ src/tumor_dataset.py
@@ -59,6 +59,11 @@ class TumorSemanticSegmentationDataset(Dataset):
                 self.image_id_to_annotation[image_id] = [annotation]
 
 
+        # THIS IS BAD, but THE DATASET IS ALSO BAD
+        INCORRECTLY_ANNOTATED_IMAGES = {1380}
+        for image_id in INCORRECTLY_ANNOTATED_IMAGES:
+            del self.image_id_to_file_name[image_id]
+        
         self.image_ids = list(self.image_id_to_file_name.keys())
 
 
