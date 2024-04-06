e2b91d72d92be9467a653152de96f12529dea886
jhrudden <johnhenryrudden@gmail.com>
2024-04-05 23:02:39 -0400

    start bayesian optimization tuning script

---
+++ src/scripts/tune_segmentation.py
@@ -0,0 +1,8 @@
+from bayes_opt import BayesianOptimization
+
+"""
+Use Bayesian Optimization to tune the hyperparameters of the segmentation model.
+1. Learning rate
+2. Batch size
+3. Maybe others ...
+"""
