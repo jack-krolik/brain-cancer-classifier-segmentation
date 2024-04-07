05494a55aefc7fab16285647418185d1bfc8c8c8
jhrudden <johnhenryrudden@gmail.com>
2024-04-07 01:32:48 -0400

    build out local logging (plotting)

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -1,9 +1,13 @@
 import pathlib
 import wandb
-import torch
+from datetime import datetime
+import numpy as np
 import uuid
 import json
 from typing import Dict
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import pandas as pd
 
 from src.utils.config import TrainingConfig
 
@@ -12,7 +16,7 @@ LOGS_DIR = pathlib.Path(__file__).parent.parent.parent / "logs"
 class LoggerMixin:
     def __init__(self, config: TrainingConfig):
         self.config = config
-
+    
     def init(self, **kwargs):
         """
         Initialize the logger with the given configuration
@@ -61,7 +65,7 @@ class WandbLogger(LoggerMixin):
         wandb.login(key=wandb_API_key, verify=True)
 
     def init(self, **kwargs):
-        self.run = wandb.init(config=self.config.flatten(), project=self.project_name, tags=self.tags, **kwargs)
+        self.run = wandb.init(config=self.config.flatten(), project=self.project_name, tags=self.tags **kwargs)
 
     def finish(self):
         if self.run:
@@ -78,23 +82,28 @@ class WandbLogger(LoggerMixin):
         self.run.save(str(path)) # Save the model to wandb
     
 class LocalLogger(LoggerMixin):
-    def __init__(self, config: TrainingConfig):
+    def __init__(self, config: TrainingConfig, run_group: str = None):
         super().__init__(config)
         self.metrics = {}
+
+        self.run_group = "_".join(run_group.split()) if run_group else 'Experiment'
 
     def init(self, **kwargs):
         # Initialize the metrics dictionary
         self.metrics = {}
 
+        self.save_dir = LOGS_DIR / self.run_group if self.run_group else LOGS_DIR
+
         # create a random seed for the run id if not provided
-        run_id = kwargs.get('run_id', None)
+        run_id = kwargs.get('name', None)
         if not run_id:
-            run_id = str(uuid.uuid4())
-            kwargs['run_id'] = run_id
+            run_id = str(uuid.uuid4())[:8]
+        
+        run_id = run_id + "_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
         # create a directory to store the logs
-        LOGS_DIR.mkdir(parents=True, exist_ok=True)
-        self.log_dir = LOGS_DIR / run_id
+        self.save_dir.mkdir(exist_ok=True)
+        self.log_dir = self.save_dir / run_id
         self.log_dir.mkdir(exist_ok=True)
 
     def finish(self):
@@ -107,5 +116,24 @@ class LocalLogger(LoggerMixin):
             step = len(self.metrics) + 1
         self.metrics[step] = metrics
 
+    
     def plot_metrics(self):
-        raise NotImplementedError("Method 'plot_metrics' not implemented.")
+        metrics_df = pd.DataFrame(self.metrics).T
+        metrics_df.index.name = "Step"
+
+        num_metrics = len(metrics_df.columns)
+        n_rows = int(np.ceil(num_metrics / 2))
+
+        fig = make_subplots(rows=n_rows, cols=2, subplot_titles=metrics_df.columns)
+
+        for i, metric in enumerate(metrics_df.columns, start=1):
+            row = (i+1) // 2
+            col = 2 if i % 2 == 0 else 1
+
+            fig.add_trace(go.Scatter(x=metrics_df.index, y=metrics_df[metric], mode='lines', name=metric), row=row, col=col)
+
+        fig.update_layout(title='Metrics', xaxis_title='Epoch', yaxis_title='Value')
+        fig.write_html(str(self.log_dir / "metrics.html"))
+        fig.show()
+
+        
